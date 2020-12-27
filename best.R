5e4b868e534a7b7871e9b0b12949a42c020b7fe3
heyben88 <benjamin.f.gerard@gmail.com>
2020-12-27 08:58:07 +0100

    best.r final - alphabetic order added

--- best.R
+++ best.R
@@ -13,41 +13,41 @@ best <- function(state, outcome){
   diseaseslist<-toupper(names(data))
   
   ## Check that state and outcome are valid
-    ## see if state exists
-    if (any(is.na(table(data$State)[State=state]))) stop("invalid state")
-    ##see if outcome exists
-    if  (!(disease) %in% diseaseslist) stop("invalid outcome")
+  ## see if state exists
+  if (any(is.na(table(data$State)[State=state]))) stop("invalid state")
+  ##see if outcome exists
+  if  (!(disease) %in% diseaseslist) stop("invalid outcome")
   
-
+  
   
   ##keep only researched state data
   data<-data[ which(data$State==state),]
   
   ##useless assign column number with the researched data i.e the outcome
   b<-which(diseaseslist==disease)
- 
-  ##keep only useful data without na (not available replacement too)
-  data<-na.omit(data.frame(data[["Hospital.Name"]],"Rate"=gsub("Not Available", NA, data[[b]])))
   
-  ##Remove NA
-  data<-na.omit(data$Rate)
-  names(data)<-c("Hospital","Rate")
+  ##keep only useful data without na (not available replacement too)
+  data<-data.frame(data[["Hospital.Name"]],"Rate"=gsub("Not Available", NA, data[[b]]))
+  
   ##transform as numeric for classification
   data$Rate = as.numeric(as.character(data$Rate))
   
   ##sorting by name (should be done on exaequo only)
-  data<-data[order(data[,1]),] 
+  ##old data<-na.omit(data[order(data[,1]),])
+  ##search for the min in 30.Day.Death..Mortality
+  ##old   bb<-which.min(data$Rate)
+  ##return result
+  ##old   data[bb,1]
   
-  ##old search for the min in 30.Day.Death..Mortality
-  ##old bb<-which.min(data$Rate)
-  ##old return result
-  ##old data[bb,1]
+  ## Taking of NA 
+  data<-na.omit(data,data$Rate)
+
+  ## Data with minimum value
+  data<-data[which(data$Rate %in% min(data$Rate)),]
+
+  data<-data[order(data[,1]),] } #Alphabetic sort 
   
-  ##data with min value
-  data<-data(which(data[,2] %in% min(data[,2])))
-  ##sorting by name (should be done on exaequo only)
-  data<-data[order(data[,1]),] 
-  data(1,1)
-  
+  #Result
+  data[1,1]
 }
-  
+
