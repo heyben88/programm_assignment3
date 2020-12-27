ecb9c6c1fb2533c2cb46e04777a8a1abc5113e84
heyben88 <benjamin.f.gerard@gmail.com>
2020-12-27 08:58:56 +0100

    first version of final rankall

---
+++ rankall.R
@@ -0,0 +1,43 @@
+rankall <- function(outcome, num = "best") {
+  ##settle work folder as the one with the data
+  setwd("C:/Users/pc/Documents/2 SEIMA/Benas/travail/data analysis/R projects/programm_assignment3")
+  ##assign data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ## Space elimination from the entered outcome
+  outcome<-sub(" ",".",outcome)
+  ##change in uppercase to avoid unwanted mismatch
+  disease<-toupper(paste("Hospital.30.Day.Death..Mortality..Rates.from.",outcome, sep = ""))
+  diseaseslist<-toupper(names(data))
+  
+  ##see if outcome exists
+  if  (!(disease) %in% diseaseslist) stop("invalid outcome")
+  
+  ## shorten data to essential to lighten the space used for memory
+  data=data[c(7,2,which(diseaseslist==disease))]
+  names(data)[3]="Rate"
+  ##keep only useful data without na (not available replacement too)
+  data[3]<-gsub("Not Available", NA, data[[3]])
+  data<-na.omit(data,data$Rate)
+  data$Rate = as.numeric(as.character(data$Rate))
+  
+            rankhospital_for_rankall <- function(data, num = "best") {
+              ##transform as numeric
+              
+              if (num=="worst") 
+              {data<-data[order(-data$Rate,data[,2]),] } ##reverse sorting to take the worth, higher first
+              else
+              {data<-data[order(data$Rate,data[,2]),] }  ##sorting lower first
+              
+              ##Top if best or worst
+              if (num=="best" || num=="worst") {num<-1} 
+              
+              ##return result
+              data[num,1:2]
+            }  
+  
+  
+  splitdata <- split(data, data$State)
+  a=lapply(splitdata, rankhospital_for_rankall)
+  print(head(a))
+}
+head(rankall("heart attack", 20), 10)
